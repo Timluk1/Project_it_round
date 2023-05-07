af8a95e4a264366162fa84845e66da1eecc8a2c9
Timluk1 <118183087+Timluk1@users.noreply.github.com>
2023-05-07 09:18:02 +0300

    Update config.py

--- start_bot/config.py
+++ start_bot/config.py
@@ -2,6 +2,6 @@ from handlers.client import Client
 from aiogram.utils import executor
 
 
-client = Client("5762130910:AAGmI6Alyh8_6OkqvoX7b1i1TZKCM3ICPas")
+client = Client("TOKEN")
 client.register_admin_handlers(client.dp)
 executor.start_polling(client.dp, skip_updates=True)
